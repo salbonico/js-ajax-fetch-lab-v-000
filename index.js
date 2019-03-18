9734d061d6c8ec5dda8f6175865d0a6676448627
Steve Albonico <salbonico@gmail.com>
2019-03-18 01:35:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,13 +2,7 @@ function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
   const token = '';
-fetch('https://api.github.com/user/repos', {
-  headers: {
-    Authorization: `token ${token}`
-  }
-})
-  .then(res => res.json())
-  .then(json => console.log(json));
+
   return '';
 }
 
